8aeca455fc517162be1161eb191a64e64d12a3cb
sunnythecode <45048308+sunnythecode@users.noreply.github.com>
2022-06-18 16:50:50 -0700

    first

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "Robot.h"
+
+#include <fmt/core.h>
+
+#include <frc/smartdashboard/SmartDashboard.h>
+
+void Robot::RobotInit() {
+  rev::CANSparkMax* rMotor = new rev::CANSparkMax(rMotorLeaderID, rev::CANSparkMax::MotorType::kBrushless);
+  rev::CANSparkMax* rMotorFollower = new rev::CANSparkMax(rMotorFollowerID, rev::CANSparkMax::MotorType::kBrushless);
+  rev::SparkMaxRelativeEncoder lEncoder = lMotor->GetEncoder();
+  rev::SparkMaxRelativeEncoder rEncoder = rMotor->GetEncoder();
+  
+}
+
+void Robot::RobotPeriodic() {
+  frc::SmartDashboard::PutNumber("lEncoder", lEncoder.GetPosition());
+  frc::SmartDashboard::PutNumber("rEncoder", rEncoder.GetPosition());
+}
+
+void Robot::AutonomousInit() {}
+
+void Robot::AutonomousPeriodic() {}
+
+void Robot::TeleopInit() {}
+
+void Robot::TeleopPeriodic() {
+  
+}
+
+void Robot::DisabledInit() {}
+
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {}
+
+void Robot::TestPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
